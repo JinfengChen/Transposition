a1960c574ada62fde82213256451a78b2de601d0
JinfengChen <jinfeng7chen@gmail.com>
2015-11-30 13:12:44 -0800

    arrow head

--- bin/Compare_RILs_SV/draw_diagram/bin/Draw_mPing_SV.pl
+++ bin/Compare_RILs_SV/draw_diagram/bin/Draw_mPing_SV.pl
@@ -22,9 +22,9 @@ my $startw=50;
 my $endw  =1100;
 my $starth=100;
 my $endh  =500;
+my $scale  = 500/5000;
 
-
-plot_deletion_two_mping($svg, $startw, $starth, 'Type I: Deletions between two nearby mPing insertions', 't.txt');
+plot_deletion_two_mping($svg, $startw, $starth, 'Type I: Deletions between two nearby mPing insertions', 'RIL_SV.txt', $scale);
 #plot_deletion_one_mping($svg, $startw+500, $starth, 'Type 2: Deletions from one mPing insertions to flanking sequence');
 #plot_deletion_one_mping($svg, $startw, $starth+350, 'Type 3: Deletions accompanied by rearrangement of flanking sequence');
 #plot_deletion_one_mping()
@@ -36,14 +36,14 @@ writesvg("mPing_SV_diagram.svg", $svg);
 ###sub functions
 sub plot_deletion_two_mping
 {
-my ($svg, $startx, $starty, $title, $plot_inf) = @_;
+my ($svg, $startx, $starty, $title, $plot_inf, $scale) = @_;
 my $plot_data = read_inf($plot_inf);
 plot_text($svg, $startx+15, $starty-50, 'start', 15, $title);
 
 foreach my $rank (keys %$plot_data){
     my $x = $startx;
     my $y = $starty + 50 + ($rank-1)*150;
-    plot_diagram_simple($svg, $plot_data->{$rank}, $x, $y);
+    plot_diagram_simple($svg, $plot_data->{$rank}, $x, $y, $scale);
 }
 }
 
@@ -64,22 +64,64 @@ foreach my $rank (keys %$plot_data){
 ################################### sub for plot diagram of one SV#####################
 sub plot_diagram_simple
 {
-my ($svg, $data, $x, $y) = @_;
+my ($svg, $data, $x, $y, $scale) = @_;
 
 #upper plot: HEG4
 plot_text($svg, $x, $y, 'start', '15', $data->{'up_header'});
 #plot_chromosome_line()
 #plot_zoom_in_mping($svg, $x, $y, 'up', $data) if exists $data->{'up_zoom_in_mping'};
 #plot_zoom_in($svg, $x, $y, 'up', $data) if exists $data->{'up_zoom_in'};
+plot_fragment_arrow($svg, $x+50, $y-10, $data->{'up_segment'}, $data->{'up_cut'}, $data->{'up_start'}, $scale);
 
 #lower plot: RILs
 plot_text($svg, $x, $y+70, 'start', '15', $data->{'down_header'});
 #plot_chromosome_line() 
 #plot_zoom_in_mping($svg, $x, $y+70, 'down', $data) if exists $data->{'down_zoom_in_mping'};
 #plot_zoom_in($svg, $x, $y+70, 'down', $data) if exists $data->{'down_zoom_in'}; 
+plot_fragment_arrow($svg, $x+50, $y+70, $data->{'down_segment'}, $data->{'down_cut'}, $data->{'down_start'}, $scale);
 
 #comparision plot
 plot_matches($svg, $x + 50, $y, $x + 50, $y+60, $data);
+}
+
+
+#plot fragment with arrow on chromosome
+#up_segment=a:28723476:28724476:arrow-right;bc:28724476:28732623:arrow-both;d:28732623-28733623:arrow-left
+sub plot_fragment_arrow
+{
+my ($svg, $x, $y, $segment, $cut, $start, $scale) = @_;
+my %frag_hash;
+my @fragment = split(';', $segment);
+for (my $i=0; $i<@fragment; $i++){
+    my ($label, $f_start, $f_end, $arrow) = split(':', $fragment[$i]);
+    my $f_start_rl = convert_position($cut, $start, $f_start, $scale, $x);
+    my $f_end_rl = convert_position($cut, $start, $f_end, $scale, $x);
+    $frag_hash{$label}{'start'} = $f_start_rl;
+    $frag_hash{$label}{'end'} = $f_end_rl;
+    print "$label\t$f_start_rl\t$f_end_rl\t$arrow\n";
+    #draw arrow on left of segment
+    if ($arrow eq 'arrow-left'){
+       my $line=$svg->line(
+           x1=>$f_start_rl+10, y1=>$y,
+           x2=>$f_end_rl, y2=>$y,
+           style=>{stroke=>'black', 'marker-start'=>'url(#left_arrow)'}
+       );
+    #draw arrow on right of segment
+    }elsif($arrow eq 'arrow-right'){
+       my $line=$svg->line(
+           x1=>$f_start_rl, y1=>$y,
+           x2=>$f_end_rl-10, y2=>$y,
+           style=>{stroke=>'black', 'marker-end'=>'url(#right_arrow)'}
+       );  
+    #draw arrow on both side of segment
+    }elsif($arrow eq 'arrow-both'){
+       my $line=$svg->line(
+           x1=>$f_start_rl+10, y1=>$y,
+           x2=>$f_end_rl-10, y2=>$y,
+           style=>{stroke=>'black', 'marker-start'=>'url(#left_arrow)', 'marker-end'=>'url(#right_arrow)'}
+       );  
+    }  
+}
 }
 
 ################################### sub for plot_strain name ##########################
@@ -316,6 +358,31 @@ my $rightline=$svg->line(
 
 }
 
+################################### convert position to relative cut point if need ####
+#cut=cutposition,cutlength or 0,0
+#up_start is the start position on chromosome
+#match_up_x1 is the position on chromosome to convert
+#scale=500/5000
+#x1 is the start position in plot area
+sub convert_position
+{
+my ($cut, $up_start, $match_up_x1, $scale, $x1) = @_;
+my ($upcut, $upcut_len) = split(',', $cut);
+if (1){
+    if ($upcut > 0){
+       if ($match_up_x1 > $upcut){
+           $match_up_x1 = ($match_up_x1 - $up_start - $upcut_len + 1)*$scale + $x1;
+       }else{
+           $match_up_x1 = ($match_up_x1 - $up_start + 1)*$scale + $x1;
+       }
+    }else{
+       $match_up_x1 = ($match_up_x1 - $up_start + 1)*$scale + $x1;
+    }
+}
+return $match_up_x1
+}
+
+
 ################################### sub for read ploting infromation###################
 sub read_inf
 {
@@ -343,8 +410,41 @@ return \%hash;
 ################################### sub for write svg to file##########################
 sub writesvg {
 my ($file, $svg)=@_;
-open OUT, ">$file" or die "can not open my file";
+open OUT, ">temp.svg" or die "can not open my file";
     print OUT $svg->xmlify;
 close OUT;
+
+my $left_arrow=<<DEF;
+      <defs>
+        <marker id="left_arrow" markerWidth="10" markerHeight="10" refX="0" refY="3" orient="180" markerUnits="strokeWidth" viewBox="0 0 10 10">
+        <path d="M0,0 L0,6 L9,3 z" fill="black" />
+        </marker>
+      </defs>
+DEF
+
+my $right_arrow=<<DEF;
+      <defs>
+        <marker id="right_arrow" markerWidth="10" markerHeight="10" refX="0" refY="3" orient="0" markerUnits="strokeWidth" viewBox="0 0 10 10">
+        <path d="M0,0 L0,6 L9,3 z" fill="black" />
+        </marker>
+      </defs>
+DEF
+
+
+open OUT, ">$file" or die "$!";
+open IN, "temp.svg" or die "$!";
+my $line = 0;
+while(<IN>){
+    chomp $_;
+    next if ($_=~/^$/ or $_ =~/^#/);
+    $line++;
+    if ($line==3){
+        print OUT "$_\n";
+        print OUT "$left_arrow\n$right_arrow\n";
+    }else{
+        print OUT "$_\n";
+    }
+}
+close IN;
 system("/rhome/cjinfeng/BigData/software/draw/svg2xxx_release/svg2xxx $file -t pdf");
 }
