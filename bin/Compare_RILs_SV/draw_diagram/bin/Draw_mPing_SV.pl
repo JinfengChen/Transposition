619959caaf7cb42d3d90563b16656448074759d0
JinfengChen <jinfeng7chen@gmail.com>
2015-11-30 21:38:38 -0800

    3 type

--- bin/Compare_RILs_SV/draw_diagram/bin/Draw_mPing_SV.pl
+++ bin/Compare_RILs_SV/draw_diagram/bin/Draw_mPing_SV.pl
@@ -17,14 +17,19 @@ if(defined $opt{help} or keys %opt < 1){
 
 
 
-my $svg=SVG->new(width=>1200,height=>800);
+my $svg=SVG->new(width=>1200,height=>1000);
 my $startw=50; 
 my $endw  =1100;
 my $starth=100;
 my $endh  =500;
 my $scale  = 500/5000;
 
-plot_deletion_two_mping($svg, $startw, $starth, 'Type I: Deletions between two nearby mPing insertions', 'RIL_SV.txt', $scale);
+print "Plot type1\n";
+plot_deletion_two_mping($svg, $startw, $starth, 'Type 1: Deletions between two nearby mPing insertions', 'RIL_SV_type1.txt', $scale);
+print "Plot type2\n";
+plot_deletion_two_mping($svg, $startw+600, $starth, 'Type 2: Deletions from one mPing insertions to flanking sequence', 'RIL_SV_type2.txt', $scale);
+print "Plot type3\n";
+plot_deletion_two_mping($svg, $startw, $starth+350, 'Type 3: Deletions accompanied by rearrangement of flanking sequence', 'RIL_SV_type3.txt', $scale);
 #plot_deletion_one_mping($svg, $startw+500, $starth, 'Type 2: Deletions from one mPing insertions to flanking sequence');
 #plot_deletion_one_mping($svg, $startw, $starth+350, 'Type 3: Deletions accompanied by rearrangement of flanking sequence');
 #plot_deletion_one_mping()
@@ -40,9 +45,10 @@ my ($svg, $startx, $starty, $title, $plot_inf, $scale) = @_;
 my $plot_data = read_inf($plot_inf);
 plot_text($svg, $startx+15, $starty-50, 'start', 15, $title);
 
-foreach my $rank (keys %$plot_data){
+foreach my $rank (sort {$a <=> $b} keys %$plot_data){
+    print "SV$rank\n";
     my $x = $startx;
-    my $y = $starty + 50 + ($rank-1)*150;
+    my $y = $starty + ($rank-1)*150;
     plot_diagram_simple($svg, $plot_data->{$rank}, $x, $y, $scale);
 }
 }
@@ -71,17 +77,32 @@ plot_text($svg, $x, $y, 'start', '15', $data->{'up_header'});
 #plot_chromosome_line()
 #plot_zoom_in_mping($svg, $x, $y, 'up', $data) if exists $data->{'up_zoom_in_mping'};
 #plot_zoom_in($svg, $x, $y, 'up', $data) if exists $data->{'up_zoom_in'};
-plot_fragment_arrow($svg, $x+50, $y-10, $data->{'up_segment'}, $data->{'up_cut'}, $data->{'up_start'}, $scale);
+my $up_fragment_hash = plot_fragment_arrow($svg, $x+50, $y-10, $data->{'up_segment'}, $data->{'up_cut'}, $data->{'up_start'}, $scale, -10);
+my $up_mping_hash    = plot_mping($svg, $x+50, $y-10, $data->{'up_zoom_in_mping'}, $data->{'up_cut'}, $data->{'up_start'}, $scale);
 
 #lower plot: RILs
 plot_text($svg, $x, $y+70, 'start', '15', $data->{'down_header'});
 #plot_chromosome_line() 
 #plot_zoom_in_mping($svg, $x, $y+70, 'down', $data) if exists $data->{'down_zoom_in_mping'};
 #plot_zoom_in($svg, $x, $y+70, 'down', $data) if exists $data->{'down_zoom_in'}; 
-plot_fragment_arrow($svg, $x+50, $y+70, $data->{'down_segment'}, $data->{'down_cut'}, $data->{'down_start'}, $scale);
+my $down_fragment_hash = plot_fragment_arrow($svg, $x+50, $y+70, $data->{'down_segment'}, $data->{'down_cut'}, $data->{'down_start'}, $scale, 15);
+my $down_mping_hash    = plot_mping($svg, $x+50, $y+70, $data->{'down_zoom_in_mping'}, $data->{'down_cut'}, $data->{'down_start'}, $scale);
 
-#comparision plot
-plot_matches($svg, $x + 50, $y, $x + 50, $y+60, $data);
+#comparison plot
+#my $up_hash = merge_hash();
+@$up_fragment_hash{keys %$up_mping_hash} = values %$up_mping_hash;
+@$down_fragment_hash{keys %$down_mping_hash} = values %$down_mping_hash;
+print "fragment position in figure\n";
+print "up\n";
+foreach my $t (keys %$up_fragment_hash){
+   print "$t\t$up_fragment_hash->{$t}->{'start'}\t$up_fragment_hash->{$t}->{'end'}\n";
+}
+print "down\n";
+foreach my $t (keys %$down_fragment_hash){
+   print "$t\t$down_fragment_hash->{$t}->{'start'}\t$down_fragment_hash->{$t}->{'end'}\n";
+}
+#plot_matches($svg, $x + 50, $y, $x + 50, $y+60, $data);
+plot_matches_by_labels($svg, $x + 50, $y, $x + 50, $y+60, $up_fragment_hash, $down_fragment_hash, $data);
 }
 
 
@@ -89,7 +110,7 @@ plot_matches($svg, $x + 50, $y, $x + 50, $y+60, $data);
 #up_segment=a:28723476:28724476:arrow-right;bc:28724476:28732623:arrow-both;d:28732623-28733623:arrow-left
 sub plot_fragment_arrow
 {
-my ($svg, $x, $y, $segment, $cut, $start, $scale) = @_;
+my ($svg, $x, $y, $segment, $cut, $start, $scale, $label_y_adj) = @_;
 my %frag_hash;
 my @fragment = split(';', $segment);
 for (my $i=0; $i<@fragment; $i++){
@@ -101,27 +122,118 @@ for (my $i=0; $i<@fragment; $i++){
     print "$label\t$f_start_rl\t$f_end_rl\t$arrow\n";
     #draw arrow on left of segment
     if ($arrow eq 'arrow-left'){
+       #line and arrow
        my $line=$svg->line(
            x1=>$f_start_rl+10, y1=>$y,
            x2=>$f_end_rl, y2=>$y,
            style=>{stroke=>'black', 'marker-start'=>'url(#left_arrow)'}
        );
+       #label
+       my $text=$svg->text(
+           x=>$f_start_rl+15, y=>$y+$label_y_adj,
+               style=>{
+                  fontsize=>'4','text-anchor'=>'start','stroke-width'=>1
+               }
+       )->cdata($label);
     #draw arrow on right of segment
     }elsif($arrow eq 'arrow-right'){
+       #line and arrow
        my $line=$svg->line(
            x1=>$f_start_rl, y1=>$y,
            x2=>$f_end_rl-10, y2=>$y,
            style=>{stroke=>'black', 'marker-end'=>'url(#right_arrow)'}
-       );  
+       );
+       #label
+       my $text=$svg->text(
+           x=>$f_end_rl-25, y=>$y+$label_y_adj,
+              style=>{            
+                 fontsize=>'4','text-anchor'=>'start','stroke-width'=>1
+              }
+       )->cdata($label);
+
     #draw arrow on both side of segment
     }elsif($arrow eq 'arrow-both'){
+       #line and arrow
        my $line=$svg->line(
            x1=>$f_start_rl+10, y1=>$y,
            x2=>$f_end_rl-10, y2=>$y,
            style=>{stroke=>'black', 'marker-start'=>'url(#left_arrow)', 'marker-end'=>'url(#right_arrow)'}
-       );  
-    }  
+       );
+       #label on start
+       my $text_s=$svg->text(
+           x=>$f_start_rl+15, y=>$y+$label_y_adj,
+           style=>{              
+               'font-size'=>'15','text-anchor'=>'start','stroke-width'=>1
+           }
+       )->cdata(substr($label, 0, 1));
+       #label on end
+       my $text_e=$svg->text(
+           x=>$f_end_rl-25, y=>$y+$label_y_adj,
+           style=>{            
+               'font-size'=>'15','text-anchor'=>'start','stroke-width'=>1
+           }
+       )->cdata(substr($label, 1, 1));
+     
+    }
+     
 }
+return \%frag_hash;
+}
+
+################################### sub for plot mping ################################
+sub plot_mping
+{
+my ($svg, $x, $y, $mping, $cut, $start, $scale) = @_;
+my %mping_hash;
+my @mpings = split(';', $mping);
+for (my $i=0; $i<@mpings; $i++){
+    my ($mping_id, $m_start, $m_strand, $m_left_seq, $m_right_seq) = split(',', $mpings[$i]);
+    my $m_end = $m_start + 500;
+    my $m_start_rl = convert_position($cut, $start, $m_start, $scale, $x);
+    my $m_end_rl = convert_position($cut, $start, $m_end, $scale, $x);
+    $mping_hash{$mping_id}{'start'} = $m_start_rl;
+    $mping_hash{$mping_id}{'end'} = $m_end_rl;
+    my $mping_box = $svg->rectangle(
+       x => $m_start_rl, y => $y-10,
+       width => $m_end_rl - $m_start_rl + 1, height => 25,
+       style=>{
+             fill=>'orange',
+             'fill-opacity' => 0.7
+       }
+    );
+    if ($m_strand eq '+'){
+        print "mping on plus strand\n";
+        my $text_plus=$svg->text(
+           x=>$m_start_rl, y=>$y+12,
+              style=>{            
+                 'font-size'=>'15', 'font-style'=>'italic','text-anchor'=>'start','stroke-width'=>1
+              }
+        )->cdata('mPing');
+        my $text=$svg->text(
+           x=>$m_start_rl, y=>$y,
+              style=>{            
+                 'font-size'=>'15', 'font-style'=>'italic','text-anchor'=>'start','stroke-width'=>1
+              }
+        )->cdata('5\'->3\'');
+    }else{
+        print "mping on minus strand\n"; 
+        my $text_minus=$svg->text(
+           x=>$m_end_rl, y=>$y-2,
+           style=>{            
+                 'font-size'=>'15', 'font-style'=>'italic','text-anchor'=>'start','stroke-width'=>1
+           },
+           transform => "rotate(180 $m_end_rl, $y)"
+        )->cdata('mPing');
+        my $text_53=$svg->text(
+           x=>$m_start_rl, y=>$y,
+              style=>{            
+                 'font-size'=>'15', 'font-style'=>'italic','text-anchor'=>'start','stroke-width'=>1
+              }
+        )->cdata('3\'<-5\'');
+
+    }
+}
+return \%mping_hash;
 }
 
 ################################### sub for plot_strain name ##########################
@@ -137,6 +249,36 @@ my $strain_name=$svg->text(
 }
 
 ################################## sub for plot match ################################
+sub plot_matches_by_labels
+{
+my ($svg, $x1, $y1, $x2, $y2, $up_hash, $down_hash, $data) = @_;
+my @matches = split(';', $data->{'match'});
+for (my $i=0; $i<@matches; $i++){
+    print "$matches[$i]\n";
+    my ($up_label, $down_label, $strand, $color) = split(':', $matches[$i]);
+    print "matching pairs: $up_label, $down_label, $strand, $color\n";
+    my $match_up_x1 = $up_hash->{$up_label}->{'start'};
+    my $match_up_x2 = $up_hash->{$up_label}->{'end'};
+    my $match_down_x1 = $down_hash->{$down_label}->{'start'};
+    my $match_down_x2 = $down_hash->{$down_label}->{'end'};
+    print "match x: $match_up_x1\t$match_up_x2\t$match_down_x1\t$match_down_x2\n";
+    print "match y: $y1\t$y1\t$y2\t$y2\n"; 
+    my $xv = [$match_up_x1,$match_up_x2,$match_down_x2,$match_down_x1];
+    my $yv = [$y1, $y1, $y2, $y2];
+    my $points =$svg->get_path(
+                     x=>$xv,y=>$yv,
+                     -type=>'polyline',
+                     -closed=>'true'
+              );
+    my $tag=$svg->polyline(
+                     %$points,
+                     style=>{
+                        fill=>$color
+                     }
+              );
+}
+}#end of function
+
 sub plot_matches
 {
 my ($svg, $x1, $y1, $x2, $y2, $data) = @_;
